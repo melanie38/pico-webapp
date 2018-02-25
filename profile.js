a48d1ec0d861136d7e2c792562ead1045baa1c3d
melanie38 <melanieharini@gmail.com>
2018-02-24 17:56:12 -0700

    variable added in js

--- profile.js
+++ profile.js
@@ -2,57 +2,44 @@ angular.module('profile', [])
 .controller('MainCtrl', [
   '$scope','$http',
   function($scope,$http){
-    $scope.timings = [];
-    $scope.violations = [];
+    $scope.name = "";
+    $scope.location = "";
+    $scope.threshold = "";
+    $scoper.phone = "";
     $scope.eci = "UKs5YQUWhvJvrky73HCs8f";
 
-    var bURL = '/sky/event/'+$scope.eci+'/eid/timing/started';
-    $scope.addTiming = function() {
-      var pURL = bURL + "?number=" + $scope.number + "&name=" + $scope.name;
-      return $http.post(pURL).success(function(data){
-        $scope.getAll();
-        $scope.number='';
-        $scope.name='';
+    var nameURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/name';
+    $scope.getName = function() {
+      return $http.get(nameURL).success(function(data){
+        angular.copy(data, $scope.name);
       });
     };
 
-    var iURL = '/sky/event/'+$scope.eci+'/eid/timing/finished';
-    $scope.finished = function(number) {
-      var pURL = iURL + "?number=" + number;
-      return $http.post(pURL).success(function(data){
-        $scope.getAll();
+    var locationURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/location';
+    $scope.getLocation = function() {
+      return $http.get(locationURL).success(function(data){
+        angular.copy(data, $scope.location);
       });
     };
 
-    var gURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/temperatures';
-    $scope.getAll = function() {
-      return $http.get(gURL).success(function(data){
-        angular.copy(data, $scope.timings);
+    var thresholdURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold';
+    $scope.getThreshold = function() {
+      return $http.get(thresholdURL).success(function(data){
+        angular.copy(data, $scope.threshold);
       });
     };
 
-    var tURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold_violations';
-    $scope.getViolations = function() {
-      return $http.get(tURL).success(function(data){
-        angular.copy(data, $scope.violations);
+    var phoneURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/phone';
+    $scope.getPhone = function() {
+      return $http.get(phoneURL).success(function(data){
+        angular.copy(data, $scope.phone);
       });
     };
 
-    $scope.getAll();
-    $scope.getViolations();
+    $scope.getName();
+    $scope.getLocation();
+    $scope.getThreshold();
+    $scope.getPhone();
 
-    $scope.timeDiff = function(timing) {
-      var bgn_sec = Math.floor(Date.parse(timing.time_out)/1000);
-      var end_sec = Math.floor(Date.parse(timing.time_in)/1000);
-      var sec_num = end_sec - bgn_sec;
-      var hours   = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-      if (hours   < 10) {hours   = "0"+hours;}
-      if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
-      return hours+':'+minutes+':'+seconds;
-    }
   }
 ]);
