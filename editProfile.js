e73b6a69e4c1f8c846b2f9940569276c24137ce4
melanie38 <melanieharini@gmail.com>
2018-02-24 23:51:52 -0700

    edit profile added

--- editProfile.js
+++ editProfile.js
@@ -5,13 +5,14 @@ angular.module('edit', [])
     $scope.eci = "UKs5YQUWhvJvrky73HCs8f";
 
     var udpateURL = 'http://35.161.218.80:8080/sky/event/'+
-      $scope.eci+'/0002/sensor/profile_updated?name='+
-      $scope.name+'&location='+
-      $scope.location+'&threshold='+
-      $scope.threshold+'&phone='+
-      $scope.phone;
+      $scope.eci+'/0002/sensor/profile_updated'
     $scope.saveProfile = function() {
-      return $http.post(udpateURL).success(function(data){
+      var url = updateURL +
+        '?name='+$scope.name+
+        '&location='+$scope.location+
+        '&threshold='+$scope.threshold+
+        '&phone='+$scope.phone;
+      return $http.post(url).success(function(data){
         $scope.name = '';
         $scope.location = '';
         $scope.threshold = 0;
