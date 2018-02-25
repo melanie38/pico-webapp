36909603c201bd9b6cff90b44175c48d11dd4f22
melanie38 <melanieharini@gmail.com>
2018-02-24 22:11:14 -0700

    changed controller name

--- profile.js
+++ profile.js
@@ -2,10 +2,10 @@ angular.module('profile', [])
 .controller('ProfileCtrl', [
   '$scope','$http',
   function($scope,$http){
-    $scope.name = "";
-    // $scope.location = "";
-    // $scope.threshold = "";
-    // $scope.phone = 0;
+    $scope.name = "Melanie";
+    $scope.location = "";
+    $scope.threshold = 0;
+    $scope.phone = "";
     $scope.eci = "UKs5YQUWhvJvrky73HCs8f";
 
     var nameURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/name';
@@ -15,31 +15,31 @@ angular.module('profile', [])
       });
     };
 
-    // var locationURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/location';
-    // $scope.getLocation = function() {
-    //   return $http.get(locationURL).success(function(data){
-    //     angular.copy(data, $scope.location);
-    //   });
-    // };
+    var locationURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/location';
+    $scope.getLocation = function() {
+      return $http.get(locationURL).success(function(data){
+        angular.copy(data, $scope.location);
+      });
+    };
 
-    // var thresholdURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold';
-    // $scope.getThreshold = function() {
-    //   return $http.get(thresholdURL).success(function(data){
-    //     angular.copy(data, $scope.threshold);
-    //   });
-    // };
-    //
-    // var phoneURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/phone';
-    // $scope.getPhone = function() {
-    //   return $http.get(phoneURL).success(function(data){
-    //     angular.copy(data, $scope.phone);
-    //   });
-    // };
+    var thresholdURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold';
+    $scope.getThreshold = function() {
+      return $http.get(thresholdURL).success(function(data){
+        angular.copy(data, $scope.threshold);
+      });
+    };
+
+    var phoneURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/phone';
+    $scope.getPhone = function() {
+      return $http.get(phoneURL).success(function(data){
+        angular.copy(data, $scope.phone);
+      });
+    };
 
     $scope.getName();
-    // $scope.getLocation();
-    // $scope.getThreshold();
-    // $scope.getPhone();
+    $scope.getLocation();
+    $scope.getThreshold();
+    $scope.getPhone();
 
   }
 ]);
