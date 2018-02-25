2919c6edc03d2c98ce7ea7a1c27fa5aad1bda938
melanie38 <melanieharini@gmail.com>
2018-02-24 22:37:59 -0700

    changed controller name

--- profile.js
+++ profile.js
@@ -2,37 +2,37 @@ angular.module('profile', [])
 .controller('ProfileCtrl', [
   '$scope','$http',
   function($scope,$http){
-    // $scope.name = "";
-    // $scope.location = "";
-    // $scope.threshold = 0;
-    // $scope.phone = "";
+    $scope.name = "Melanie";
+    $scope.location = "";
+    $scope.threshold = 0;
+    $scope.phone = "";
     $scope.eci = "UKs5YQUWhvJvrky73HCs8f";
 
     var nameURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/name';
     $scope.getName = function() {
       return $http.get(nameURL).success(function(data){
-        angular.copy(data, $scope.name);
+        $scope.name = data;
       });
     };
 
     var locationURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/location';
     $scope.getLocation = function() {
       return $http.get(locationURL).success(function(data){
-        angular.copy(data, $scope.location);
+        $scope.location = data;
       });
     };
 
     var thresholdURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/threshold';
     $scope.getThreshold = function() {
       return $http.get(thresholdURL).success(function(data){
-        angular.copy(data, $scope.threshold);
+        $scope.threshold = data;
       });
     };
 
     var phoneURL = 'http://35.161.218.80:8080/sky/cloud/'+$scope.eci+'/temperature_store/phone';
     $scope.getPhone = function() {
       return $http.get(phoneURL).success(function(data){
-        angular.copy(data, $scope.phone);
+        $scope.phone = data;
       });
     };
 
